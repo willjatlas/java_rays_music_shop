bc5183c35e2e32e9337e2964a992847b03fa891c
William Anderson <willjatlas@googlemail.com>
2021-02-06 15:24:44 +0000

    IPlay tests finished

--- src/test/java/Instruments/AcousticGuitarTest.java
+++ src/test/java/Instruments/AcousticGuitarTest.java
@@ -1,4 +1,28 @@
 package Instruments;
 
+import Instruments.Guitars.AcousticGuitar;
+import Instruments.Guitars.ElectricGuitar;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class AcousticGuitarTest {
+
+    AcousticGuitar aG;
+
+    @Before
+    public void before(){
+        aG = new AcousticGuitar("Fender", "CD60", "Natural", "Gloss",
+                64.33, 119.00, 149.00, false,
+                "Laminate", "Laminate", "Laminate", "Aluminium",
+                "Dreadnought", 6, "None", 76.5,
+                false);
+    }
+
+    @Test
+    public void canPlayGuitar(){
+        assertEquals("Some delicious acoustic", aG.makeSound());
+    }
+
 }
--- src/test/java/Instruments/BassGuitarTest.java
+++ src/test/java/Instruments/BassGuitarTest.java
@@ -1,4 +1,27 @@
 package Instruments;
 
+import Instruments.Guitars.BassGuitar;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BassGuitarTest {
+
+    BassGuitar bG;
+
+    @Before
+    public void before(){
+        bG = new BassGuitar("Yamaha", "RBX170", "Metallic Red", "Gloss",
+                112.87, 189.00, 249.00, false,
+                "Basswood", "Mahogany", "Rosewood", "Aluminium",
+                "RBX", 4, "Yamaha", "Standard",
+                2, "Long");
+    }
+
+    @Test
+    public void canPlayGuitar(){
+        assertEquals("Slappin da bass", bG.makeSound());
+    }
+
 }
